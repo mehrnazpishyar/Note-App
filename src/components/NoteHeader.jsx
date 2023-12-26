f6706812da3898b794502bef0776c6a5a0ae4fdf
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-12-26 19:56:30 +0100

    add css for responsive

--- src/components/NoteHeader.jsx
+++ src/components/NoteHeader.jsx
@@ -4,7 +4,7 @@ const NoteHeader = ({ notes, sortBy, onSort }) => {
  
   return (
     <div className="note-header">
-      <h1>My Notes ({notes.length})</h1>
+      <p>My Notes ({notes.length})</p>
       <select value={sortBy} onChange={onSort}>
         <option value="latest">Sort based on latest notes</option>
         <option value="earliest">Sort based on earliest notes</option>
