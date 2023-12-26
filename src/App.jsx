10de11d8a28b77fd9b8cb431ce25ebf34b6e8c08
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-12-26 17:20:08 +0100

    create NoteStatus component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import AddNewNote from "./components/AddNewNote";
 import NoteList from "./components/NoteList";
+import NoteStatus from "./components/NoteStatus";
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -30,6 +31,7 @@ function App() {
       <div className="note-app">
         <AddNewNote onAddNote={handleAddNote} />
         <div className="note-container">
+          <NoteStatus notes={notes}/>
           <NoteList
             notes={notes}
             onDelete={handleDeleteNote}
